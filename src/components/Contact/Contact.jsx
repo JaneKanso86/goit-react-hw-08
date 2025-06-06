8f2f16ae444905ad4deccfa03692b82b7c8ca126
Jane Kanso <janekanso86@gmail.com>
2025-06-06 22:07:19 +0300

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsOps';
+import css from './Contact.module.css';
+import { FaUser, FaPhone } from 'react-icons/fa';
+
+export default function Contact({ contact: { id, name, number } }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <div className={css.card}>
+      <div>
+        <p>
+          <FaUser className={css.icon} />
+          {name}
+        </p>
+        <p>
+          <FaPhone className={css.icon} />
+          {number}
+        </p>
+      </div>
+      <div>
+        <button className={css.button} onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,49 @@
+import { useDispatch } from 'react-redux';
+import css from './ContactForm.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { addContact } from '../../redux/contactsOps';
+
+export default function ContactForm() {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    const form = evt.target;
+    if (
+      form.elements.name.value.trim() == '' ||
+      form.elements.number.value.trim() == ''
+    ) {
+      toast.error('Please fill both fields.');
+      return;
+    }
+
+    dispatch(
+      addContact({
+        id: crypto.randomUUID(),
+        name: form.elements.name.value,
+        number: form.elements.number.value,
+      }),
+    );
+    form.reset();
+  };
+
+  return (
+    <div className={css.card}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" className={css.field} />
+
+        <label htmlFor="number">Number</label>
+        <input type="text" name="number" id="number" className={css.field} />
+
+        <button type="submit" className={css.button}>
+          Add contact
+        </button>
+      </form>
+
+      <ToastContainer position="top-right" autoClose={3000} />
+    </div>
+  );
+}
